2fbaba37b46b7f721c595e3553579ee6ef9a5731
natalya <bushueva09@yandex.ru>
2025-12-01 13:17:50 +0100

    first msg 3

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import		os
+import	os
 from fastapi import FastAPI, Request
 from aiogram import Bot, Dispatcher
 from aiogram.types import Update
 
 from bot	import bot, dp
 
-		WEBHOOK_URL = os.
-getenv("WEBHOOK_URL")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 app = FastAPI()
+
 @ app.on_event("startup")
 async def on_startup():
-await bot.set_webhook(WEBHOOK_URL)
+	await bot.set_webhook(WEBHOOK_URL)
+
 @ app.post("/webhook")
 async def telegram_webhook(request: Request):
-data = await request.json()
-update = Update(**data)
-await dp.feed_update(bot, update)
-return {"ok":True}
+	data = await request.json()
+	update = Update(**data)
+	await dp.feed_update(bot, update)
+	return {"ok":True}
